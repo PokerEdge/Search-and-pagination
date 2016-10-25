0382c0bb61ab6d96aec9493b6fd378069d5a2a57
Reid Young <reid@pokersprout.com>
2016-10-25 13:57:20 -0400

    Improved search function and removed all comments
    Search function displays elements within the active page anchor element.
    
    Comments have been removed for easier reading and cleaner workspace.
    
    Issue remains that search results with 37 elements display 4 pages of
    10 elements, all of which are displaying 10 elements (40 elements total
    are displayed when only 37 elements should be displayed)

--- js/test.js
+++ js/test.js
@@ -1,15 +1,15 @@
+//CHANGE NAME TO MAIN.JS WITH WORKING CODE - ONLY ISSUE IS PAGINATION OF NEW SEARCH RESULTS
+
+//What is grouping the displayed students into groups of 10? That needs to happen after each search()
+	//It's the startIndex and endIndex loop series --> they require some conditional to display search results, if any
+
 
 var searchResultArray = [];
 
 var numShownElements;
 
-var startIndex;
-
-var endIndex;
-
 
 $(function(){
-
 
 	$(".page-header.cf").append("<div class='student-search'><input placeholder='Search for students...'><button>Search</button>");
 
@@ -22,6 +22,8 @@ $(function(){
 
 function init(arr){
 
+	//If numShowElements can be defined w function scope, then there only need be an 'if' condition
+	//When no results are found, sra needs to return undefined
 	if (searchResultArray.length === 0){
 
 		numShownElements = $(".student-list").children().length;
@@ -47,43 +49,56 @@ function init(arr){
 	var $pageUl = $("<ul></ul>");
 
 	for(var j = 0; j < totalPages; j++){
-
+	
 		$anchorItem = $('<a href="#">' + (j+1) + '</a>');
 
 		$(".pagination").append($pageUl);
 		
+		//begin anonymous function that binds click handler and should show(s) 10 elements per page
 		$anchorItem.click(function(){
 
 			$(".pagination a").removeClass("active");
 			$(this).addClass("active");
 
-
 			lastPageNumber = currentPageNumber;
 
 			currentPageNumber = parseInt($(this).text());
 
-			startIndex = currentPageNumber * 10 - 10;
+			var startIndex = currentPageNumber * 10 - 10;
 
-			endIndex = currentPageNumber * 10;
+			var endIndex = currentPageNumber * 10;
+
 
 			for(var i = 0; i < $(".student-list").children().length; i++){
+			  
 			  $(".student-list li").eq(i).hide();
-
-			  if(i >= startIndex && i < endIndex){
-
+			  
+			  //HOW WILL NEW SEARCH RESULTS BECOME PAGINATED?
+			  if(i >= startIndex && i < endIndex && searchResultArray === 0){ // add an and statement here or another 'if' conditional
 			  	$(".student-list li").eq(i).show();
 			  }
+
+			    else if (i >= startIndex && i < endIndex && searchResultArray[i] !== undefined){//WHAT SHOULD BE SHOWN IS FROM SEARCHRESULTSARRAY && EXISTS
+			    	
+			    	//Shows all of the student-list li as indexed by the searchResultsArray passed from search()
+			    	$(".student-list li").eq(searchResultArray[i]).show();
+
+			    } else{ //no search results displayed
+
+			    	//display "no search results displayed message"
+			    	console.log("no search results displayed");
+			    }
 			}
+		}); //end anonymous function
 
-		});
 
+		//THIS PLACEMENT SEEMS TO BE INCORRECT
 		$listItem.appendTo($pageUl);
 		$anchorItem.appendTo($listItem);
 
 		$(".pagination a").eq(0).addClass("active");
 
 	}
-
 
 	numShownElements = $(".student-list").children().length;
 
@@ -94,10 +109,14 @@ function search(){
 
 	var $input = $(".student-search input");
 
+	console.log($input.val());
+
+
   	$userNames = $(".student-details h3");
 
  	$emails = $("span.email");
 
+ 	//searchResultArray reset to prepare for new or continued search (entire value of 'input' is searched)
  	searchResultArray = [];
 
 	for(var i = 0; i < $(".student-list").children().length; i++){
@@ -106,23 +125,18 @@ function search(){
 
 	    if($userNames.eq(i).text().toLowerCase().indexOf($input.val().toLowerCase()) !== -1 || $emails.eq(i).text().toLowerCase().indexOf($input.val().toLowerCase()) !== -1){
 
-	      if(i >= startIndex && i < endIndex){
+	      $(".student-list li").eq(i).show();
 
-			  	$(".student-list li").eq(i).show();
-			  }
-	        
-	        
 	      searchResultArray.push(i);
-
+  
 	    }
 
 	}
 
-
 	console.log("Search result count is " + searchResultArray.length);
-
+	
 	return init(searchResultArray); 
+
 }
 
 init();
-
