20c5eb84633ed326d4e1f010b5396d95064cfcee
Reid Young <reid@pokersprout.com>
2016-10-30 19:25:30 -0400

    showTen() function working
    showTen() is a function that displays 10 student-list elements for the
    properly indexed page WHEN CLICKED.
    
    Remaining issue is that function fails if page is not clicked, e.g.
    when init() is called post search() call. Results are all returned
    elements displayed on page 1, until another anchor element is clicked,
    including anchor element for page 1.

--- js/main.js
+++ js/main.js
@@ -35,6 +35,8 @@ function init(arr){
 
 	}
 
+	var initCount = 0;
+
 	var lastPageNumber;
 	var currentPageNumber = 1;
 
@@ -53,72 +55,102 @@ function init(arr){
 		$anchorItem = $('<a href="#">' + (j+1) + '</a>');
 
 		$(".pagination").append($pageUl);
-		
-		$anchorItem.click(function(){
 
-			$(".pagination a").removeClass("active");
-			$(this).addClass("active");
-
-			lastPageNumber = currentPageNumber;
-
-			currentPageNumber = parseInt($(this).text());
-
-			var startIndex = currentPageNumber * 10 - 10;
-
-			var endIndex = currentPageNumber * 10;
-
-			$('.student-list').children().hide();
-
-			for(var i = 0; i < numShownElements.length; i++){ 
-			
-			console.log(startIndex + " " + endIndex);
-			if(i >= startIndex && i < endIndex){
-
-				console.log(numShownElements[i]);
-
-			  	$('.student-list').children().eq(numShownElements[i]).show();
-			  	//Add counter here to check for no results displayed
-
-			}
-		}
-
-		});
+		$anchorItem.click(showTen);
 
 		$listItem.appendTo($pageUl);
 		$anchorItem.appendTo($listItem);
 
-		$(".pagination a").eq(0).addClass("active");
-
 	}
-	// $(".pagination a").eq(0).addClass("active");
+
+		//ONLY REMAINING ISSUE IS THAT SEARCH RESULTS ARE ALL DISPLAYED IN A SINGLE PAGE - 
+			//CLICKING ANCHOR WORKS THOUGH
+			//NEED FUNCTION TO INIT FIRST 10 (SEARCH) ELEMENTS OR CODE FOR IT HERE?
+					
+
+		//Displaying first 10 elements should not be within the for loop
+
+			// $('.student-list').children().eq(numShownElements[i]).show();
+			// initCount++;
+
+			// if(initCount > 10){
+
+			// 	//ISSUE HERE? on init after search, isn't displaying search results, does on click of 1 though
+			// 	$(".student-list li").eq(numShownElements[i]).hide();
+				
+			// }
+
+	$(".pagination a").eq(0).addClass("active");
+}
+
+
+
+
+function showTen(){
+
+	var displayCount = 0;
+
+	var currentPageNumber = parseInt($(this).text());
+
+	var lastPageNumber = currentPageNumber;
+
+	var startIndex = currentPageNumber*10-10;
+
+	var endIndex = currentPageNumber*10;
+
+	$(".pagination a").removeClass("active");
+			
+	$(this).addClass("active");
+
+	$('.student-list').children().hide();
+
+	for(var i = 0; i < numShownElements.length; i++){
+			
+		console.log(startIndex + " " + endIndex);
+		if(i >= startIndex && i < endIndex){
+
+			console.log(numShownElements[i]);
+
+			//ISSUE IS HERE
+			$('.student-list').children().eq(numShownElements[i]).show();
+			displayCount++;
+
+			if(displayCount > 10){
+
+				//ISSUE HERE? on init after search, isn't displaying search results, does on click of 1 though
+				$(".student-list li").eq(numShownElements[i]).hide();
+				
+			}
+		}
+	}
 }
 
 
 function search(){
 
+	//Issue going from selected page '$(this)' to search() results, e.g. user clicks page 3 then searches
+		//Post search() function call, not all items are being hidden? Too many are being shown?
+
+	searchResultArray = [];
 
 	var $input = $(".student-search input");
 
 	//Logs the 'input' value to the console
 	console.log($input.val());
 
-
   	$userNames = $(".student-details h3");
 
  	$emails = $("span.email");
-
- 	searchResultArray = [];
 	
 
 	for(var i = 0; i < $(".student-list").children().length; i++){
 
 	    if($userNames.eq(i).text().toLowerCase().indexOf($input.val().toLowerCase()) !== -1 || $emails.eq(i).text().toLowerCase().indexOf($input.val().toLowerCase()) !== -1){
 
-			$(".student-list li").eq(i).show();
+			//SOMETHING IS INCORRECT HERE BECAUSE AFTER SEARCH DISPLAYS FIRST 14 ELEMENTS
+			// $(".student-list li").eq(i).show();
 	        
 	      	searchResultArray.push(i);
-
-	      // searchResultArray[i].style.display = "block";
 
 	    }
 
@@ -126,7 +158,7 @@ function search(){
 
 	console.log("Search result count is " + searchResultArray.length);
 
-	return init(searchResultArray); 
+	return init(searchResultArray);
 
 }
 
