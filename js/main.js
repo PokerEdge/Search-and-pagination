04aaf521b83a264233c9846d33325afed1cbcbb5
Reid Young <reid@pokersprout.com>
2016-11-07 15:05:50 -0500

    Several updates and features added
    ‘No search results’ message is shown if no search results
    
    ‘No search results’ message is hidden if there are search results
    
    Animations occur when new page elements are created and new search
    results are found
    
    Search function is called when user keys up while the ‘input’ text
    field is selected
    
    Plain language descriptive comments added throughout the script to
    communicate the purpose of the script easier
    
    ————
    
    Remaining issues and questions are commented out atop the script

--- js/main.js
+++ js/main.js
@@ -1,165 +1,144 @@
-//search() is called and no results are displayed after paginate(searchResultArray) is returned
-	//pagination elements are correctly made after search() is called, but results are not displayed
+/******************************************************************************************
 
-//on click of any anchor element, including 1, results are displayed properly
+An outstanding issue is that if a user deletes 'input' text that paginate reads 'list' as a DOM element
+	based on the current logic of its conditions to check for that (namely the comparison of length of 
+	student-item and length of 'list'). So how can this be reconciled while also checking if the search()
+	results yield a match (length is not 0)?
+
+Another problematic use case that fails is that if 'i' is typed twice quickly, "No results message" is 
+	shown, along with a few single 'i' results. If the characters are typed slower, then the script 
+	functions as intended. Perhaps this is simply a shortcoming of using JavaScript for animations, 
+	rather than using CSS3 with newer browsers, and something that cannot be fixed using jQuery?
+
+What are ways in which this code might be made more efficient, cleaner and (for lack of a better way to say),
+	more experienced in function and in appearance?
+
+Thank YOU so much for the time you're taking for the detailed review. I appreciated it so very much - Reid	
+
+******************************************************************************************/
 
 
-var searchResultArray = [];
-
-var numShownElements = [];
-
-
+//Initialization of some elements of the page on page load 
 $(function(){
 
-	$(".page-header.cf").append("<div class='student-search'><input placeholder='Search for students...'><button>Search</button>");
+	//Dynamically insert both the 'input' line and the search button onto the page
+	$('.page-header.cf').append("<div class='student-search'><input placeholder='Search for students...'><button>Search</button>");
 
-	$(".student-search button").click(function(){ search(); });
+	//When a key is let up while 'input' is selected, the search function is called
+	$('.student-search input').keyup(function(){ search(); });
 
-	// $(".student-search input").keyup(function(){ search(); });
+	//Styled "No search results found" type of message is added to the end of the page
+	$('.page').append("<div class='message'>No student elements are being shown.</div>");
+	
+	//Hide "No search results found" type of message on page load
+	$('.message').hide();
 
 });
 
 
-function paginate(arr, page){
+//Show range of ten student-items on specified page
+function paginate(list, page){
 
+	//Defining starting and stopping points for page specific 10 element groups
+	var startIndex = page*10-10;
+	var endIndex = page*10;
 
-	if (searchResultArray.length === 0){
+	//Create new page links
+   	createLinks(list);
 
-		for(var a = 0; a < $(".student-list").children().length; a++){
-			
-			numShownElements.push(a);
-		}
+   	//Add active class to 'page' argument of paginate()
+	$('.pagination a').eq(page-1).addClass('active');
+
+	//Hide all student elements to ready for conditional to display matched elements, if any match
+	$('.student-list').children().hide();
+
+	for(var i = 0; i < list.length; i++){ 
 		
+		//Condition to sort 10 element group indecies by page given index
+		if(i >= startIndex && i < endIndex){
+
+			//Populate 'list' checked against 'list' being a DOM element or not so that paginate() functions
+			if(list.length < $('.student-item').length){
+
+				//Animate in matched objects with short time delay
+				$('.student-item').eq(list[i]).delay(25*i).fadeIn('fast');
+
+			} else{
+
+				//Animate in matched objects with short time delay
+				list.eq(i).delay(25*i).fadeIn('fast');
+			}
+		}
 	}
-	else{
+}
 
-		numShownElements = searchResultArray;
 
-	}
+//Create numbered anchor elements depending on number of elements in 'list'
+function createLinks(list){
 
-	// var displayCount = 0;
+	//Calculate the total number of page elements to be created
+    var totalPages = Math.ceil(list.length/10);
 
-	var lastPageNumber;
-	var currentPageNumber = 1;
-
-	var totalPages = Math.ceil(numShownElements.length/10);
-
-	var $listItem = $("<li></li>");
+    //Get empty elements to construct the new page elements
+    var $pageUl = $("<ul></ul>");
+    var $listItem = $("<li></li>");
 	var $anchorItem;
 
-	$(".pagination").detach();
+	//Remove all .pagination and children elements from the page so that new ones will be created separately
+	$(".pagination").remove();
 
+	//Begin creation of new page elements
 	$(".page").append('<div class = "pagination"></div>');
-	var $pageUl = $("<ul></ul>");
+	$(".pagination").append($pageUl);
 
 	for(var j = 0; j < totalPages; j++){
 	
+		//Create anchor item with specific page number text based on total number of page elements to be created
 		$anchorItem = $('<a href="#">' + (j+1) + '</a>');
 
-		$(".pagination").append($pageUl);
+		//Bind handler to paginate() on particular anchor element click
+		$anchorItem.click(function(){ paginate(list, parseInt($(this).text()));});
 
-		
-		//ONLY REMAINING ISSUE IS THAT SEARCH RESULTS ARE ALL DISPLAYED IN A SINGLE PAGE - 
-			//CLICKING ANCHOR WORKS THOUGH
-		$anchorItem.click(showTen);
-
+		//Append elements to other elements in the page to build a paginated student element 'list'
 		$listItem.appendTo($pageUl);
 		$anchorItem.appendTo($listItem);
-
-	}
-
-
-//Does not work for the issue it should solve
-
-	// if (searchResultArray.length < ('student-list').children().length){
-		
-	// 	displayCount++;
-
-	// 	if(displayCount > 10){
-
-	// 		//need to also hide elements less than i  - this needs to be index filtered to count to 10
-	// 		$(".student-list li").eq(numShownElements[i]).hide();
-				
-	// 	}
-	// } else{
-
-	// 	while(displayCount < 10){
-
-	// 		$(".student-list li").eq(numShownElements[i]).show();
-	// 		displayCount++;
-		
-	// 	}
-	// }
-
-
-
-	$(".pagination a").eq(0).addClass("active");
-
-}
-
-function showTen(){
-
-	var currentPageNumber = parseInt($(this).text());
-
-	var lastPageNumber = currentPageNumber;
-
-	var startIndex = currentPageNumber*10-10;
-
-	var endIndex = currentPageNumber*10;
-
-	$(".pagination a").removeClass("active");
-			
-	$(this).addClass("active");
-
-	$('.student-list').children().hide();
-
-	for(var i = 0; i < numShownElements.length; i++){ 
-			
-		console.log(startIndex + " " + endIndex);
-		if(i >= startIndex && i < endIndex){
-
-			console.log(numShownElements[i]);
-
-			$('.student-list').children().eq(numShownElements[i]).show();
-		}
 	}
 }
 
-
+//
 function search(e){
-
-	//Issue going from selected page '$(this)' to search() results, e.g. user clicks page 3 then searches
-		//Post search() function call, not all items are being hidden? Too many are being shown?
-
-	searchResultArray = [];
-
+   
+	//Get HTML values with jQuery and store them in variables for manipulation and for search
 	var $input = $(".student-search input");
+	var $userNames = $(".student-details h3");
+ 	var $emails = $("span.email");
 
-	//Logs the 'input' value to the console
-	console.log($input.val());
+	//Variable that stores indices that are matched by search function conditions
+	var searchResultArray = [];
 
-  	$userNames = $(".student-details h3");
+	//Hide message for when user begins new search after "No results shown" message has been shown
+	$('.message').hide();
 
- 	$emails = $("span.email");
-	
-
-	for(var i = 0; i < $(".student-list").children().length; i++){
+	for(var i = 0; i < $(".student-item").length; i++){
 
 	    if($userNames.eq(i).text().toLowerCase().indexOf($input.val().toLowerCase()) !== -1 || $emails.eq(i).text().toLowerCase().indexOf($input.val().toLowerCase()) !== -1){
-
-			//SOMETHING IS INCORRECT HERE BECAUSE AFTER SEARCH DISPLAYS FIRST 14 ELEMENTS
-			// $(".student-list li").eq(i).show();
 	        
 	      	searchResultArray.push(i);
 
 	    }
+	}
+
+	//Show "No search results displayed" message if no criteria are matched from 'list'
+	if(searchResultArray.length === 0){
+
+		$('.message').show();
 
 	}
 
-	console.log("Search result count is " + searchResultArray.length);
-
-	return paginate(searchResultArray, 1); 
+    //Call pagination function with arguments that are a 'list' of an array of indices and 'page' of 1
+    paginate(searchResultArray, 1);
 
 }
 
-paginate();
+//Run script
+paginate($('.student-item'), 1);
--- js/main_copy.js
+++ js/main_copy.js
@@ -1,122 +1,144 @@
-var numShownElements = [];
+/******************************************************************************************
 
-var searchResultArray = [];
+An outstanding issue is that if a user deletes 'input' text that paginate reads 'list' as a DOM element
+	based on the current logic of its conditions to check for that (namely the comparison of length of 
+	student-item and length of 'list'). So how can this be reconciled while also checking if the search()
+	results yield a match (length is not 0)?
 
+Another problematic use case that fails is that if 'i' is typed twice quickly, "No results message" is 
+	shown, along with a few single 'i' results. If the characters are typed slower, then the script 
+	functions as intended. Perhaps this is simply a shortcoming of using JavaScript for animations, 
+	rather than using CSS3 with newer browsers, and something that cannot be fixed using jQuery?
+
+What are ways in which this code might be made more efficient, cleaner and (for lack of a better way to say),
+	more experienced in function and in appearance?
+
+Thank YOU so much for the time you're taking for the detailed review. I appreciated it so very much - Reid	
+
+******************************************************************************************/
+
+
+//Initialization of some elements of the page on page load 
 $(function(){
 
-	$(".page-header.cf").append("<div class='student-search'><input placeholder='Search for students...'><button>Search</button>");
+	//Dynamically insert both the 'input' line and the search button onto the page
+	$('.page-header.cf').append("<div class='student-search'><input placeholder='Search for students...'><button>Search</button>");
 
-	$(".student-search button").click(search);
+	//When a key is let up while 'input' is selected, the search function is called
+	$('.student-search input').keyup(function(){ search(); });
 
-	// $(".student-search input").keyup(function(){ search(); });
+	//Styled "No search results found" type of message is added to the end of the page
+	$('.page').append("<div class='message'>No student elements are being shown.</div>");
+	
+	//Hide "No search results found" type of message on page load
+	$('.message').hide();
 
 });
 
 
+//Show range of ten student-items on specified page
 function paginate(list, page){
 
-	// Show range of ten items on specified page
+	//Defining starting and stopping points for page specific 10 element groups
 	var startIndex = page*10-10;
-
 	var endIndex = page*10;
 
-	// Also have to create new page links, but lets think about that elsewhere
+	//Create new page links
    	createLinks(list);
 
-	$(".pagination a").eq(page-1).addClass("active");
+   	//Add active class to 'page' argument of paginate()
+	$('.pagination a').eq(page-1).addClass('active');
 
+	//Hide all student elements to ready for conditional to display matched elements, if any match
 	$('.student-list').children().hide();
 
 	for(var i = 0; i < list.length; i++){ 
-			
-		console.log(startIndex + " " + endIndex);
 		
+		//Condition to sort 10 element group indecies by page given index
 		if(i >= startIndex && i < endIndex){
 
+			//Populate 'list' checked against 'list' being a DOM element or not so that paginate() functions
 			if(list.length < $('.student-item').length){
-				$('.student-item').eq(list[i]).show();
+
+				//Animate in matched objects with short time delay
+				$('.student-item').eq(list[i]).delay(25*i).fadeIn('fast');
+
 			} else{
-				list.eq(i).show();
+
+				//Animate in matched objects with short time delay
+				list.eq(i).delay(25*i).fadeIn('fast');
 			}
 		}
 	}
-
-	// return nothing
-
 }
 
 
+//Create numbered anchor elements depending on number of elements in 'list'
 function createLinks(list){
-	
 
-	//create properly numbered anchor elements
+	//Calculate the total number of page elements to be created
     var totalPages = Math.ceil(list.length/10);
 
+    //Get empty elements to construct the new page elements
     var $pageUl = $("<ul></ul>");
     var $listItem = $("<li></li>");
 	var $anchorItem;
 
-	$(".pagination").detach();
+	//Remove all .pagination and children elements from the page so that new ones will be created separately
+	$(".pagination").remove();
 
+	//Begin creation of new page elements
 	$(".page").append('<div class = "pagination"></div>');
 	$(".pagination").append($pageUl);
 
 	for(var j = 0; j < totalPages; j++){
 	
+		//Create anchor item with specific page number text based on total number of page elements to be created
 		$anchorItem = $('<a href="#">' + (j+1) + '</a>');
 
-		//bind click handler to search() on anchor elements, $(this) isn't passing?
-		$anchorItem.click(function(){ paginate(list, parseInt($(this).text())); });
+		//Bind handler to paginate() on particular anchor element click
+		$anchorItem.click(function(){ paginate(list, parseInt($(this).text()));});
 
+		//Append elements to other elements in the page to build a paginated student element 'list'
 		$listItem.appendTo($pageUl);
 		$anchorItem.appendTo($listItem);
-
 	}
-
 }
 
-
+//
 function search(e){
    
-	// Get all results that match input from the complete 'list'
-	searchResultArray = [];
-
+	//Get HTML values with jQuery and store them in variables for manipulation and for search
 	var $input = $(".student-search input");
-
-	//Logs the 'input' value to the console
-	console.log($input.val());
-
-  	var $userNames = $(".student-details h3");
-
+	var $userNames = $(".student-details h3");
  	var $emails = $("span.email");
-	
+
+	//Variable that stores indices that are matched by search function conditions
+	var searchResultArray = [];
+
+	//Hide message for when user begins new search after "No results shown" message has been shown
+	$('.message').hide();
 
 	for(var i = 0; i < $(".student-item").length; i++){
 
-
 	    if($userNames.eq(i).text().toLowerCase().indexOf($input.val().toLowerCase()) !== -1 || $emails.eq(i).text().toLowerCase().indexOf($input.val().toLowerCase()) !== -1){
 	        
-	        //Not being registered... somehow..., though for loop is passing values correctly
 	      	searchResultArray.push(i);
 
 	    }
+	}
+
+	//Show "No search results displayed" message if no criteria are matched from 'list'
+	if(searchResultArray.length === 0){
+
+		$('.message').show();
 
 	}
 
-	// if(searchResultArray.length === 0){
-
-	// 	console.log("No search results available");
-
-	// }
-
-	console.log("Search result count is " + searchResultArray.length);
-
-    // run the pagination function with the results array and page = 1
+    //Call pagination function with arguments that are a 'list' of an array of indices and 'page' of 1
     paginate(searchResultArray, 1);
-
-    // return nothing
-    	//nothing returned
 
 }
 
+//Run script
 paginate($('.student-item'), 1);
